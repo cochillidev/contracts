f53a168bf2ee2f04162d79530e0df61150380c80
cochilli <138674364+cochilli@users.noreply.github.com>
2023-08-14 12:14:33 +0000

    update imports

--- src/TestToken.sol
+++ src/TestToken.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "openzeppelin/token/ERC20/ERC20.sol";
-import "openzeppelin/token/ERC20/extensions/ERC20Burnable.sol";
-import "openzeppelin/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "openzeppelin-contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "openzeppelin-contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
 contract TestToken is ERC20, ERC20Burnable, ERC20Permit {
     constructor() ERC20("Test Token", "USDC") ERC20Permit("Test Token") {
